ed4066fb142c50d20f55991623ad811f204965e0
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2022-11-18 07:01:07 -0600

    Update Config.php
    update database info

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,5 @@ namespace silverorange\DevTest;
 
 class Config
 {
-    //public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
-     public string $dsn = 'mysql:host=localhost;dbname=silverorange _db;user=root;password=';
-
+    public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange'; 
 }
